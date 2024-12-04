3f4de08a121a1e73cd4f0aafca23ce4ab238df4c
Barbora <tarcakova3@stud.uniza.sk>
2024-12-04 22:01:29 +0100

    extracting javascript for image/url switch into separate file

--- App/Views/root.layout.view.php
+++ App/Views/root.layout.view.php
@@ -83,8 +83,8 @@
         </p>
     </div>
 </footer>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="public/js/script.js"></script>
 </body>
 </html>
 
